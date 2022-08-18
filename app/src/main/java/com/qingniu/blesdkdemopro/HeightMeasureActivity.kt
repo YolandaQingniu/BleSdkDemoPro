f6770ede55d28df46f21e02d17f102b64975a35b
HYR <huangyiran@yolanda.hk>
2022-08-18 17:00:18 +0800

    update(main):修改测量界面

--- app/src/main/java/com/qingniu/blesdkdemopro/HeightMeasureActivity.kt
+++ app/src/main/java/com/qingniu/blesdkdemopro/HeightMeasureActivity.kt
@@ -15,10 +15,14 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
 import com.qingniu.blesdkdemopro.ui.theme.BgGrey
 import com.qingniu.blesdkdemopro.ui.theme.BleSdkDemoProTheme
 import com.qingniu.blesdkdemopro.ui.widget.TitleBar
@@ -28,6 +32,7 @@ import com.qingniu.qnheightweightscaleplugin.listener.QNHeightWeightScaleStatusL
 import com.qingniu.qnheightweightscaleplugin.model.*
 import com.qingniu.qnplugin.QNPlugin
 import com.qingniu.qnplugin.inter.QNResultCallback
+
 class HeightMeasureActivity : ComponentActivity() {
 
     companion object {
@@ -35,6 +40,8 @@ class HeightMeasureActivity : ComponentActivity() {
             return Intent(ctx, HeightMeasureActivity::class.java)
         }
     }
+
+    val mHeightScaleViewModel = HeightScaleViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,8 +57,20 @@ class HeightMeasureActivity : ComponentActivity() {
                             .fillMaxSize()
                             .background(BgGrey)
                     ) {
+                        val weightS = remember {
+                            mHeightScaleViewModel.weight
+                        }
+                        val heightS = remember {
+                            mHeightScaleViewModel.height
+                        }
+                        val weightUnitS = remember {
+                            mHeightScaleViewModel.weightUnit
+                        }
+                        val heightUnitS = remember {
+                            mHeightScaleViewModel.heightUnit
+                        }
                         TitleBar("Height Scale", true)
-                        MeasureBoard(weight = 56.5, unit = "kg")
+                        MeasureBoard(weightS, heightS, weightUnitS, heightUnitS)
                     }
                 }
             }
@@ -69,6 +88,7 @@ class HeightMeasureActivity : ComponentActivity() {
                 }
             })
         QNHeightWeightScalePlugin.setDeviceListener {
+            QNPlugin.getInstance(this).stopScan()
             Log.e("qzx", it.toString())
             val operate = QNHeightWeightScaleOperate()
             operate.heightUnit = QNHeightUnit.QNHeightUnitCm
@@ -108,28 +128,42 @@ class HeightMeasureActivity : ComponentActivity() {
         })
         QNHeightWeightScalePlugin.setDataListener(object : QNHeightWeightScaleDataListener {
             override fun onHeightWeightScaleRealTimeWeight(
-                weight: String?,
-                device: QNHeightWeightScaleDevice?
+                weight: String,
+                device: QNHeightWeightScaleDevice
             ) {
                 Log.e("qzx", "onHeightWeightScaleRealTimeWeight, weight = $weight")
+                mHeightScaleViewModel.apply {
+                    this.weight.value = weight
+                }
             }
 
             override fun onHeightWeightScaleRealTimeHeight(
-                height: String?,
-                device: QNHeightWeightScaleDevice?
+                height: String,
+                device: QNHeightWeightScaleDevice
             ) {
                 Log.e("qzx", "onHeightWeightScaleRealTimeHeight, height = $height")
+                mHeightScaleViewModel.apply {
+                    this.height.value = height
+                }
             }
 
             override fun onHeightWeightScaleReceiveMeasureResult(
-                scaleData: QNHeightWeightScaleData?,
-                device: QNHeightWeightScaleDevice?
+                scaleData: QNHeightWeightScaleData,
+                device: QNHeightWeightScaleDevice
             ) {
                 Log.e("qzx", "onHeightWeightScaleReceiveMeasureResult, result = $scaleData")
+                mHeightScaleViewModel.apply {
+                    this.weight.value = scaleData.weight
+                    this.height.value = scaleData.height
+                }
             }
 
             override fun onHeightWeightScaleReceiveMeasureFailed(device: QNHeightWeightScaleDevice?) {
                 Log.e("qzx", "onHeightWeightScaleReceiveMeasureFailed")
+                mHeightScaleViewModel.apply {
+                    this.weight.value = "--"
+                    this.height.value = "--"
+                }
             }
 
         })
@@ -143,21 +177,25 @@ class HeightMeasureActivity : ComponentActivity() {
 }
 
 @Composable
-fun MeasureBoard(weight: Double, unit: String) {
-    val showText = if (weight > 0) {
-        "$weight $unit"
-    } else {
-        "- - $unit"
-    }
-
+fun MeasureBoard(
+    weight: MutableState<String>, height: MutableState<String>,
+    weightUnit: MutableState<String>, heightUnit: MutableState<String>
+) {
     Column(
         Modifier
             .padding(top = 160.dp)
             .fillMaxSize()
     ) {
         Text(
-            text = showText, fontSize = 32.sp,
+            text = "${weight.value} ${weightUnit.value} ${height.value} ${heightUnit.value}", fontSize = 32.sp,
             modifier = Modifier.align(Alignment.CenterHorizontally)
         )
     }
+}
+
+class HeightScaleViewModel:ViewModel(){
+    var weight: MutableState<String> = mutableStateOf("--")
+    var height: MutableState<String> = mutableStateOf("--")
+    var weightUnit: MutableState<String> = mutableStateOf("KG")
+    var heightUnit: MutableState<String> = mutableStateOf("CM")
 }
