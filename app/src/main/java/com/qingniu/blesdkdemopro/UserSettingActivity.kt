d5c13ec3f23bb3480da9f0a1f178be8b6d38fbfc
HYR <huangyiran@yolanda.hk>
2022-09-14 21:26:36 +0800

    update(1.1.0):修改年龄

--- app/src/main/java/com/qingniu/blesdkdemopro/UserSettingActivity.kt
+++ app/src/main/java/com/qingniu/blesdkdemopro/UserSettingActivity.kt
@@ -3,19 +3,23 @@ package com.qingniu.blesdkdemopro
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.qingniu.blesdkdemopro.constant.DemoUnit
@@ -45,6 +49,9 @@ class UserSettingActivity : ComponentActivity() {
                     val ctx = LocalContext.current
                     val dao = DemoDataBase.getInstance(ctx).userDao()
 
+                    val age = remember {
+                        mutableStateOf(dao.getUser().age)
+                    }
                     val gender = remember {
                         mutableStateOf(dao.getUser().gender)
                     }
@@ -53,13 +60,27 @@ class UserSettingActivity : ComponentActivity() {
                             .background(BgGrey)
                             .fillMaxSize()
                     ) {
-                        TitleBar(title = "Select Unit")
+                        TitleBar(title = "Age")
                         Column(
                             Modifier.padding(top = 20.dp)
                         ) {
                             Text(
                                 text = "Weight Unit", fontSize = 16.sp,
                                 modifier = Modifier.padding(start = 10.dp, bottom = 10.dp)
+                            )
+                            TextField(value = age.value.toString(), onValueChange = {
+                                if (!TextUtils.isEmpty(it)) {
+                                    age.value = it.toInt()
+
+                                } else {
+                                    age.value = 0
+                                }
+                                val user = dao.getUser().apply {
+                                    this.age = age.value
+                                }
+                                dao.update(user)
+                            },
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
                             )
                         }
                         Column(Modifier.padding(top = 20.dp)) {
@@ -68,18 +89,18 @@ class UserSettingActivity : ComponentActivity() {
                                 modifier = Modifier.padding(start = 10.dp, bottom = 10.dp)
                             )
                             SelectUnitItem(unit = DemoUnit.MALE.showName, checkState = { gender.value == DemoUnit.MALE.showName }) {
-                                val unit = dao.getUser().apply {
+                                val user = dao.getUser().apply {
                                     this.gender = DemoUnit.MALE.showName
                                 }
-                                dao.update(unit)
-                                gender.value = unit.gender
+                                dao.update(user)
+                                gender.value = user.gender
                             }
                             SelectUnitItem(unit = DemoUnit.FEMALE.showName, checkState = { gender.value == DemoUnit.FEMALE.showName }) {
-                                val unit = dao.getUser().apply {
+                                val user = dao.getUser().apply {
                                     this.gender = DemoUnit.FEMALE.showName
                                 }
-                                dao.update(unit)
-                                gender.value = unit.gender
+                                dao.update(user)
+                                gender.value = user.gender
                             }
                         }
                     }
